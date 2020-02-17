5dc9c006d3ff4a30d50e4cb5d4edf408d2a749eb
Achintya0502 <achintyapatel55555@gmail.com>
2020-02-18 00:24:21 +0530

    adding and removing users

--- public/js/chat.js
+++ public/js/chat.js
@@ -35,6 +35,7 @@ socket.on('message', (message) => {
     console.log(message)
 
     const html = Mustache.render(messageTemplate, {
+        username: message.username,
         message: message.text,
         createdAt: moment(message.createdAt).format('h:mm a') 
     })
@@ -43,6 +44,7 @@ socket.on('message', (message) => {
 
 socket.on('locationMessage', (message) => {
     const html = Mustache.render(locationMessageTemplate, {
+        username: message.username,
         url: message.url,
         createdAt: moment(message.createdAt).format('h:mm a')
     })
@@ -67,4 +69,9 @@ $sendLocationButton.addEventListener('click', () => {
     })
 })
 
-socket.emit('join', {username, room})
+socket.emit('join', {username, room}, (error) => {
+    if(error){
+        alert(error)
+        location.href = '/'
+    }
+})
